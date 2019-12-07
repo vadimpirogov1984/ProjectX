f01a1b2e1eda8e14a89f25651a18563804806765
Artem <58067191+frostnn@users.noreply.github.com>
2019-12-07 09:43:16 +0300

    add js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-$(function() {
-    //карусель отзывов
+$(function () {
+  $('.multiple-review').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    centerMode: true,
+    centerPadding: "0",
+    // responsive: [
+    //   {
+    //     breakpoint: 768,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1,
+    //       centerMode: false,
+    //     }
+    //   },
 
-$('.center').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});
 
-});
+    // ]
+  });
+});	
